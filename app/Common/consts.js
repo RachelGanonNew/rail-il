8b86a92ce2f4991144fec23ceec16caf1c238b31
RachelGanonNew <r05458410430@gmail.com>
2021-07-25 00:42:01 +0300

    fetch api + multipile

--- app/Common/consts.js
+++ app/Common/consts.js
@@ -12,8 +12,8 @@ export const CATEGORY = 'Movie category';
 export const PLOT = 'Movie description';
 
 export const POP="POP";
-// export const API_KRY = '11c0d1b4';
-// export const OMD_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KRY}`;
+export const API_KRY = '11c0d1b4';
+export const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KRY}`;
 
-const API_KEY = `2vG2AA9QISQP3AyO5fAGuwNLayZcYSFm`;
-export const API_URL = `https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=${API_KEY}`;
+// const API_KEY = `2vG2AA9QISQP3AyO5fAGuwNLayZcYSFm`;
+// export const API_URL = `https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=${API_KEY}`;
--- app/containers/MoviesListPage/saga.js
+++ app/containers/MoviesListPage/saga.js
@@ -2,21 +2,44 @@ import { call, put } from 'redux-saga/effects';
 import request from 'utils/request';
 import { LOAD_MOVIES } from './constants';
 import { movieLoaded, movieLoadingError } from './actions';
- import { API_URL } from '../../Common/consts';
+import { API_URL } from '../../Common/consts';
 
-const baseUrl = "/api";
+// const baseUrl = "/api";
 
 
 export function* getMovies() {
-  const requestURL = `${baseUrl}/movies/list`;
-  // const requestURL = `${API_URL}`;
+  // const requestURL = `${baseUrl}/movies/list`;
+  const requestURL = `${API_URL}`;
   try {
     const list = yield call(request, requestURL);
-    yield put(movieLoaded(list));
+    const res = initList(list);
+    yield  put(movieLoaded(res));
   } catch (err) {
     yield put(movieLoadingError(err));
   }
 }
 export default function* loadMoviesData() {
   yield (LOAD_MOVIES, getMovies());
+}
+
+const initList = (list) => {
+  console.log('list in init',list);
+  const res = [];
+  let i = 0;
+  while(i<6){
+    const currentMovie = {
+      Director: list.Director,
+      Genre: list.Genre,
+      Plot: list.Plot,
+      Poster: list.Poster,
+      Rated: 0,
+      Title: list.Title,
+      Writer: list.Writer,
+      Year: list.Year,
+      id: i+1
+    }
+    res[i] = currentMovie;
+    i +=1;
+  }
+  return res;
 }
